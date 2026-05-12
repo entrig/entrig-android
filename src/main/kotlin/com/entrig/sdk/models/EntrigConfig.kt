bc4806eebf586bab1b07f8912dc5b21dd03d3d14
ib <team@entrig.com>
2026-05-12 15:21:45 +0530

    update showForegroundNotification defaults to false

--- src/main/kotlin/com/entrig/sdk/models/EntrigConfig.kt
+++ src/main/kotlin/com/entrig/sdk/models/EntrigConfig.kt
@@ -7,14 +7,14 @@ package com.entrig.sdk.models
  * @property handlePermission If true, SDK will automatically request notification permission on registration (Android 13+)
  * @property notificationChannelId Custom notification channel ID (default: "entrig_default")
  * @property notificationChannelName Custom notification channel name shown in settings (default: "General")
- * @property showForegroundNotification If true, notifications will be displayed when app is in foreground (default: true)
+ * @property showForegroundNotification If true, notifications will be displayed when app is in foreground (default: false)
  */
 data class EntrigConfig(
     val apiKey: String,
     val handlePermission: Boolean = true,
     val notificationChannelId: String = "entrig_default",
     val notificationChannelName: String = "General",
-    val showForegroundNotification: Boolean = true
+    val showForegroundNotification: Boolean = false
 ) {
     init {
         require(apiKey.isNotEmpty()) { "API key cannot be empty" }
